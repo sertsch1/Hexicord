0ba11fc5d4815b371047ecae92d6d95003b61787
Maks Mazurov (fox.cpp) <foxcpp@yandex.ru>
2017-08-18 20:57:46 +0300

    Lowered min. CMake version & system beast support.
    + cmake/external_targets.cmake
    Macroses for creation of targets for libraries if not created by
    CMake Find* modules (CMake 3.4- for OpenSSL, CMake 3.5- for boost).
    
    * CMakeLists.txt
    Check if we found boost.beast in system, if not - use submodule
    lib/beast.
    
    Lowered required CMake version to 3.1. However I'm not sure if it will
    work correctly.

---
+++ cmake/external-targets.cmake
@@ -0,0 +1,35 @@
+macro(define_boost_target component_name)
+    add_library(__Boost_${component_name} INTERFACE)
+    target_include_directories(__Boost_${component_name} INTERFACE ${Boost_INCLUDE_DIRS})
+    target_link_libraries(__Boost_${component_name} INTERFACE ${Boost_${component_name}_LIBRARY})
+
+    add_library(Boost::${component_name} ALIAS __Boost_${component_name})
+endmacro()
+
+macro(define_openssl_crypto)
+    add_library(__OpenSSL_Crypto INTERFACE)
+    target_include_directories(__OpenSSL_Crypto INTERFACE ${OPENSSL_INCLUDE_DIRS})
+    if(${CMAKE_VERSION} VERSION_LESS 3.2)
+        # FindOpenSSL prior to 3.2 doesn't provides separate variable libcrypto
+        target_link_libraries(__OpenSSL_Crypto INTERFACE ${OPENSSL_LIBRARIES})
+    else()
+        target_link_libraries(__OpenSSL_Crypto INTERFACE ${OPENSSL_CRYPTO_LIBRARY})
+    endif()
+
+    add_library(OpenSSL::Crypto ALIAS __OpenSSL_Crypto)
+endmacro()
+
+
+macro(define_openssl_ssl)
+    add_library(__OpenSSL_SSL INTERFACE)
+    target_include_directories(__OpenSSL_SSL INTERFACE ${OPENSSL_INCLUDE_DIRS})
+    if(${CMAKE_VERSION} VERSION_LESS 3.2)
+        # FindOpenSSL prior to 3.2 doesn't provides separate variable libssl
+        target_link_libraries(__OpenSSL_SSL INTERFACE ${OPENSSL_LIBRARIES})
+    else()
+        target_link_libraries(__OpenSSL_SSL INTERFACE ${OPENSSL_SSL_LIBRARY})
+    endif()
+
+    add_library(OpenSSL::SSL ALIAS __OpenSSL_SSL)
+endmacro()
+
