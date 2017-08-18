3b26764bc310c3222c0beabcda7eb2017f8f8bc8
Maks Mazurov (fox.cpp) <foxcpp@yandex.ru>
2017-08-18 22:13:32 +0300

    Added define_threads_target macro (for CMake 3.0)

--- cmake/external-targets.cmake
+++ cmake/external-targets.cmake
@@ -33,3 +33,15 @@ macro(define_openssl_ssl_target)
     add_library(OpenSSL::SSL ALIAS __OpenSSL_SSL)
 endmacro()
 
+macro(define_threads_target)
+    add_library(Threads::Threads INTERFACE IMPORTED)
+
+    if(THREADS_HAVE_PTHREAD_ARG)
+      set_property(TARGET Threads::Threads PROPERTY INTERFACE_COMPILE_OPTIONS "-pthread")
+    endif()
+
+    if(CMAKE_THREAD_LIBS_INIT)
+      set_property(TARGET Threads::Threads PROPERTY INTERFACE_LINK_LIBRARIES "${CMAKE_THREAD_LIBS_INIT}")
+    endif()
+endmacro()
+
