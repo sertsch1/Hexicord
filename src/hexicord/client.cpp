5138f1d9994688c838ec928f9f5edc7741b9dab3
Maks Mazurov (fox.cpp) <foxcpp@yandex.ru>
2017-08-20 04:36:33 +0300

    Report real OS in Identify payload.

--- src/hexicord/client.cpp
+++ src/hexicord/client.cpp
@@ -9,6 +9,21 @@
     #define DEBUG_MSG(msg)
 #endif
 
+// All we need is C++11 compatibile compiler and boost libraries so we can probably run on a lot of platforms.
+#if defined(__linux__) || defined(__linux) || defined(linux) || defined(__GNU__)
+    #define OS_STR "linux"
+#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+    #define OS_STR "bsd"
+#elif defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
+    #define OS_STR "win32"
+#elif defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__)
+    #define OS_STR "macos"
+#elif defined(__unix__) || defined (__unix) || defined(_XOPEN_SOURCE) || defined(_POSIX_SOURCE)
+    #define OS_STR "unix"
+#else
+    #define OS_STR "unknown"
+#endif 
+
 namespace Hexicord {
     Client::Client(boost::asio::io_service& ioService, const std::string& token) 
         : restConnection(new REST::GenericHTTPConnection<BeastHTTPS>("discordapp.com", ioService))
@@ -92,7 +107,7 @@ namespace Hexicord {
         nlohmann::json message = {
             { "token" , token },
             { "properties", {
-                { "os", "linux" }, // TODO: determine using preprocessing magic
+                { "os", OS_STR },
                 { "browser", "hexicord" },
                 { "device", "hexicord" }
             }},
