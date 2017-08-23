b9473a4495ff4009ed0feb3852eb968dfb4243d9
HexwellC <hexwellc@rape.lol>
2017-08-23 04:24:50 +0300

    Improved cmake behaviour
    Fixed GitHub link definition
    Now putting libraries to link through variable (will be used in future to link zlib library optionally)
    Fixed and improved version_decect.cmake behaviour, now using PURE MAGIC (git tags actually) to detect version

--- cmake/version_detect.cmake
+++ cmake/version_detect.cmake
@@ -2,6 +2,11 @@ if(DEFINED HEXICORD_VERSION)
     message("Skipping version detection using Git tags...")
     add_definitions(-DHEXICORD_VERSION="${HEXICORD_VERSION}")
 else()
+    set(HEXICORD_VERSION 1.0.0)
+
+    # Yeah it's a pure magic
+    project(version_detect)
+
     find_package(Git QUIET)
     if(GIT_EXECUTABLE)
         execute_process(
@@ -13,21 +18,18 @@ else()
                 OUTPUT_STRIP_TRAILING_WHITESPACE
                 ERROR_STRIP_TRAILING_WHITESPACE
         )
-    if (EXISTS ${PROJECT_SOURCE_DIR}/.git)
-            include(${PROJECT_SOURCE_DIR}/cmake/version_from_git.cmake)
+        if (EXISTS ${PROJECT_SOURCE_DIR}/.git)
+        include(${PROJECT_SOURCE_DIR}/cmake/version_from_git.cmake)
             version_from_git(GIT_EXECUTABLE ${GIT_EXECUTABLE}
-                             INCLUDE_HASH ON
-                             )
+                    INCLUDE_HASH ON
+                    )
             add_definitions(-DHEXICORD_VERSION="${SEMVER}")
             set(HEXICORD_VERSION ${VERSION})
-        else()
-            message(WARNING "Setting version to null string, because building outside of Git repository, please specify version by setting HEXICORD_VERSION CMake variable.")
-            add_definitions(-DHEXICORD_VERSION="unknown")
-            set(HEXICORD_VERSION 0.0.0)
-        endif()
     else()
-        message(WARNING "Setting version to null string, because Git executable not found, please specify version by setting HEXICORD_VERSION CMake variable.")
-        add_definitions(-DHEXICORD_VERSION="unknown")
-        set(HEXICORD_VERSION 0.0.0)
+        message(WARNING "Setting version to default string, because building outside of Git repository, please specify version by setting HEXICORD_VERSION CMake variable.")
     endif()
+    else()
+        message(WARNING "Setting version to default string, because Git executable not found, please specify version by setting HEXICORD_VERSION CMake variable.")
+    endif()
+    add_definitions(-DHEXICORD_VERSION="${HEXICORD_VERSION}")
 endif()
