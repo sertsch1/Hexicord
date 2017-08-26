23d09497312bfb1411f78ff7e955bd9625c3bb35
Maks Mazurov (fox.cpp) <foxcpp@yandex.ru>
2017-08-27 01:20:34 +0300

    Added DEBUG_MSG to RatelimitLock.
    Also fixed wrong route key in RatelimitLock::refreshInfo.

--- src/hexicord/ratelimit_lock.cpp
+++ src/hexicord/ratelimit_lock.cpp
@@ -3,6 +3,13 @@
 #include <thread>
 #include <ctime>
 
+#ifndef NDEBUG // TODO: Replace with flag that affects only Hexicord.
+    #include <iostream>
+    #define DEBUG_MSG(msg) do { std::cerr << "ratelimit_lock.cpp:" << __LINE__ << " " << (msg) << '\n'; } while (false)
+#else
+    #define DEBUG_MSG(msg)
+#endif
+
 int Hexicord::RatelimitLock::remaining(const std::string& route) {
     auto it = ratelimits.find(route);
     return it != ratelimits.end() ? it->second.remaining : -1;
@@ -26,7 +33,13 @@ void Hexicord::RatelimitLock::down(const std::string& route, std::function<void(
 
     --it->second.remaining;
 
+    DEBUG_MSG(std::string("Ratelimit semaphore acquire for route ") + route +
+              " total=" + std::to_string(it->second.total) + 
+              ", remaining=" + std::to_string(it->second.remaining));
+
     if (it->second.remaining == 0) {
+        DEBUG_MSG(std::string("Ratelimit hit for route ") + route + ", blocking until " +
+                  std::to_string(it->second.resetTime));
         while (time(nullptr) <= it->second.resetTime) {
             busyWaiter();
             std::this_thread::sleep_for(std::chrono::milliseconds(500)); 
@@ -39,5 +52,9 @@ void Hexicord::RatelimitLock::refreshInfo(const std::string& route,
                                           unsigned total,
                                           time_t   resetTime) {
 
-    ratelimits["route"] = { remaining, total, resetTime };
+    DEBUG_MSG(std::string("Route ") + route +
+              ": remaining=" + std::to_string(remaining) +
+              ", total=" + std::to_string(total) +
+              ", resetTime=" + std::to_string(resetTime));
+    ratelimits[route] = { remaining, total, resetTime };
 }
