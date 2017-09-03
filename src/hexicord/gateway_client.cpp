5017ddfaf95ad32d4c367086a8dc91b8daa625ed
HexwellC <hexwellc@rape.lol>
2017-09-04 00:05:45 +0300

    Getting rid from bloated zlibcomplete library and using own wrapper

--- src/hexicord/gateway_client.cpp
+++ src/hexicord/gateway_client.cpp
@@ -23,7 +23,8 @@
 #include <hexicord/config.hpp>
 #include <hexicord/internal/utils.hpp>
 #ifdef HEXICORD_ZLIB
-#include <zlc/zlibcomplete.hpp>
+#include <zlib.h>
+#define ZLIB_COMPLETE_CHUNK 16384
 #endif
 
 #if defined(HEXICORD_DEBUG_LOG)
@@ -48,6 +49,60 @@
     #define OS_STR "unknown"
 #endif
 
+#ifdef HEXICORD_ZLIB
+class ZLibDecompressor {
+public:
+    ZLibDecompressor() {
+        int retval;
+        strm_.zalloc = Z_NULL;
+        strm_.zfree = Z_NULL;
+        strm_.opaque = Z_NULL;
+        strm_.avail_in = 0;
+        strm_.next_in = Z_NULL;
+        retval = inflateInit2(&strm_, 15);
+        if (retval != Z_OK) {
+            DEBUG_MSG("Something gone wrong during zlib initialization!")
+        }
+    }
+
+    ~ZLibDecompressor() {
+        inflateEnd(&strm_);
+    }
+
+    uint8_t* decompress(const std::vector<uint8_t>& input) {
+        int retval;
+        std::vector<uint8_t> result(ZLIB_COMPLETE_CHUNK);
+        for (uint32_t i = 0; i < input.size(); i += ZLIB_COMPLETE_CHUNK) {
+            int howManyLeft = input.size() - i;
+            int howManyWanted = (howManyLeft > ZLIB_COMPLETE_CHUNK) ? ZLIB_COMPLETE_CHUNK : howManyLeft;
+            memcpy(in_, input.data() + i, howManyWanted);
+            strm_.avail_in = howManyWanted;
+            strm_.next_in = (Bytef *) in_;
+            if (strm_.avail_in == 0) {
+                break;
+            }
+            do {
+                memset(&out_, 0, ZLIB_COMPLETE_CHUNK);
+                strm_.avail_out = ZLIB_COMPLETE_CHUNK;
+                strm_.next_out = (Bytef *) out_;
+                retval = inflate(&strm_, Z_NO_FLUSH);
+                if (retval == Z_STREAM_ERROR) {
+                    DEBUG_MSG("zlib stream error!")
+                }
+                for (int j = 0; j < ZLIB_COMPLETE_CHUNK; j++) {
+                    result.push_back(out_[j]);
+                }
+            } while (strm_.avail_out == 0);
+        }
+        return result.data();
+    }
+private:
+    char in_[ZLIB_COMPLETE_CHUNK];
+    char out_[ZLIB_COMPLETE_CHUNK];
+    z_stream strm_;
+};
+#endif
+
 namespace Hexicord {
 
 GatewayClient::GatewayClient(boost::asio::io_service& ioService, const std::string& token)
@@ -62,9 +117,12 @@ nlohmann::json GatewayClient::parseGatewayMessage(const std::vector<uint8_t>& ms
     if (msg[0] == '{') {
         return nlohmann::json::parse(msg);
     }
-    static zlibcomplete::ZLibDecompressor decompressor;
-    string input(msg.data(), msg.size());
-    return nlohmann::json::parse(decompressor.decompress(input));
+    uint8_t* carr;
+    {
+        ZLibDecompressor decompressor = ZLibDecompressor();
+        carr = decompressor.decompress(msg);
+    }
+    return nlohmann::json::parse(carr);
 #else
     return nlohmann::json::parse(msg);
 #endif
